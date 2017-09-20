47e0457a38fbda33b6c65bf069c5ce344a5d53d4
mhoppenrath <mmhoppenrath@gmail.com>
2017-09-19 20:19:26 -0500

    Server files with all paths

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var fs = require("fs");
 var path = require('path');
+var table = require("./data/table-data.js");
+var waitinglist = require("./data/waitinglist-data.js");
 
 
 var app = express();
@@ -17,15 +19,20 @@ var tables = [{
 app.get("/home", function(req, res) {
   res.sendFile(path.join(__dirname +'/home.html'))
 });
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname +'/home.html'))
-});
 app.get("/reserve", function(req,res) {
   res.sendFile(path.join(__dirname +'/reserve.html'))
 });
 app.get("/tables", function(req,res) {
   res.sendFile(path.join(__dirname +'/tables.html'))
 });
+app.get("/api/tables", function(req,res) {
+  res.json(table);
+});
+app.get("/api/waitlist", function(req,res) {
+  res.json(waitinglist);
+});
+
+
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
