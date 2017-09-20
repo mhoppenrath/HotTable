e3309b4be2bcf03ec2fadfea93e75241d0fdbb48
chadobryhim <chadobryhim@gmail.com>
2017-09-19 19:45:04 -0500

    <>

---
+++ data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+  {
+    customerName: 'DayMan',
+    customerEmail: 'Fighter@NightMan.com',
+    customerID: 'KarateMaster',
+    phoneNumber: '123-456-7890'
+  }
+];
+
+module.exports = waitingArray;
---
+++ routing/html-routes.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports - function (app) {
+
+  app.get('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../tables.html'));
+  });
+
+  app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../reserve.html'));
+  });
+
+  app.use('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../home.html'));
+  });
+
+}
